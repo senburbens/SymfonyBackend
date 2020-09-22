35bd8169cbd5dbe779b265a0c014a52508e9777e
pierre rubens milorme <pierrerubens.milorme@hopitalweb.ht>
2020-09-22 10:48:00 -0400

    modif .env

--- migrations/Version20200922143944.php
+++ migrations/Version20200922143944.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200921140751 extends AbstractMigration
+final class Version20200922143944 extends AbstractMigration
 {
     public function getDescription() : string
     {
